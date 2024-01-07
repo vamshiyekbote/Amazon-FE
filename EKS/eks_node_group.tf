180620f2b1fdbcd3f29af48deda811250bd9e88c
RajkumarJakkula33 <149739168+RajkumarJakkula33@users.noreply.github.com>
2024-01-07 03:59:43 -0500

    Update eks_node_group.tf

--- EKS/eks_node_group.tf
+++ EKS/eks_node_group.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "kubectl-server" {
 
 resource "aws_eks_node_group" "node-grp" {
   cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "pc-node-group"
+  node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.worker.arn
   subnet_ids      = [aws_subnet.public-1.id, aws_subnet.public-2.id]
   capacity_type   = "ON_DEMAND"
