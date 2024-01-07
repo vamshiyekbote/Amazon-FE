ffd03c1841a28c2b7d48fe7b044fd2f6d939c0ac
RajkumarJakkula33 <149739168+RajkumarJakkula33@users.noreply.github.com>
2024-01-07 00:10:50 -0500

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-project16"  # Replace with your actual S3 bucket name
+    bucket = "statefile-eks-amazon"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
-    region = "us-east-2" 
+    region = "us-east-1" 
   }
 }
